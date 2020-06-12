3b21067d9cc9d3211c65eb170d9d12de65f086e4
Seremo <dawidkornec@o2.pl>
2020-06-12 02:55:20 +0200

    Revert "df"
    This reverts commit 18e91a237ce6dbcc04b3a1278a3cd5d9867ba1eb.

--- EngineX-Pro/dllmain.cpp
+++ EngineX-Pro/dllmain.cpp
@@ -32,7 +32,6 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
 
 		"SEREMO TO HUJ";
 		const char* test = "testowo";
-		DWORD sizeTwojejStarej = 45345;
 		MainCore::Crack();
 		
 		
